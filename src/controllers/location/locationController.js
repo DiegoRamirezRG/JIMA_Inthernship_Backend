fec892fc581e61470f652ac4e156ea83d4074893
DiegoRamirezRG <dieghoramreyes@gmail.com>
2023-08-31 13:10:21 -0600

    Finished the local UniversalAPI

--- src/controllers/location/locationController.js
+++ src/controllers/location/locationController.js
@@ -1,9 +1,66 @@
-
+const LocationModel = require('../../models/location/locationModel');
 
 module.exports = {
 
     async getCountries(req, res, next){
-        
+        try {
+            const countries = await LocationModel.getCountries();
+            return res.status(201).json({
+                success: true,
+                message: 'Datos obtenidos con exito',
+                data: countries
+            });
+        } catch (error) {
+            return res.status(500).json({
+                success: false,
+                message: 'Ha ocurrido un error al obtener los paises',
+                error: error.message
+            })
+        }
+    },
+
+    async getStates(req, res, next){
+        try {
+            
+            const country = req.params.countryName;
+
+            const states = await LocationModel.getStates(country);
+
+            return res.status(201).json({
+                success: true,
+                message: 'Datos obtenidos con exito',
+                data: states
+            });
+
+        } catch (error) {
+            return res.status(500).json({
+                success: false,
+                message: 'Ha ocurrido un error al obtener los estados',
+                error: error.message
+            })
+        }
+    },
+
+    async getCities(req, res, next){
+        try {
+            
+            const state = req.params.stateName;
+
+            const cities = await LocationModel.getCities(state);
+
+            return res.status(201).json({
+                success: true,
+                message: 'Datos obtenidos con exito',
+                data: cities
+            });
+
+        } catch (error) {
+            return res.status(500).json({
+                success: false,
+                message: 'Ha ocurrido un error al obtener las ciudades',
+                error: error.message
+            })
+        }
     }
 
 }
--- src/routes/location/locationRoute.js
+++ src/routes/location/locationRoute.js
@@ -4,5 +4,7 @@ const LocationController = require('../../controllers/location/locationControlle
 module.exports = (app) => {
 
     app.get('/api/location/getCountries', middleware, LocationController.getCountries);
+    app.get('/api/location/getStates/:countryName', middleware, LocationController.getStates);
+    app.get('/api/location/getCities/:stateName', middleware, LocationController.getCities);
 
 }
